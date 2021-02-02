ece9601883cf4b17cbd3eb9cc7a6ea83f0459db1
joecotterchio <joecotterchio@gmail.com>
2021-02-02 14:37:53 -0500

    scaffolding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./Components/Main";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="demo-big-content">
+      <Layout>
+        <Header
+          className="header-color"
+          title={
+            <Link style={{ textDecoration: "none", color: "white" }} to="/">
+              My Portfolio
+            </Link>
+          }
+          scroll
         >
-          Learn React
-        </a>
-      </header>
+          <Navigation>
+            <Link to="/Resume">Resume</Link>
+            <Link to="/Aboutme">About Me</Link>
+            <Link to="/Projects">Projects</Link>
+            <Link to="/Contact">Contact</Link>
+          </Navigation>
+        </Header>
+        <Drawer
+          title={
+            <Link style={{ textDecoration: "none", color: "black" }} to="/">
+              My Portfolio
+            </Link>
+          }
+        >
+          <Navigation>
+            <Link to="/Resume">Resume</Link>
+            <Link to="/Aboutme">About Me</Link>
+            <Link to="/Projects">Projects</Link>
+            <Link to="/Contact">Contact</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content"></div>
+          <Main />
+        </Content>
+      </Layout>
     </div>
   );
 }
